d4000cae71415863aa09d4facada7c8673e360ef
tsuzakitakehiro <tsuzakitakehiro@team-lab.com>
2019-04-01 18:01:57 +0900

    コードフォーマット

--- pages/index.vue
+++ pages/index.vue
@@ -2,21 +2,11 @@
   <section class="container">
     <div>
       <app-logo/>
-      <h1 class="title">
-        zakimotodev
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
+      <h1 class="title">zakimotodev</h1>
+      <h2 class="subtitle">Nuxt.js project</h2>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
+        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
       </div>
     </div>
   </section>
@@ -42,7 +32,8 @@ export default {
 }
 
 .title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif; /* 1 */
   display: block;
   font-weight: 300;
   font-size: 100px;
